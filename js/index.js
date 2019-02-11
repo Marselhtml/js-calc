02af079f8f602d497fc96b33e7f1441c3ceb9979
Daniil <daniil.shatkivskyy@gmail.com>
2019-02-11 17:35:47 +0000

    Calc

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits button, .opers button')
+     .forEach( button => button.addEventListener('click', pressDigitOrOper));
+
+function pressDigitOrOper(e){
+ display.value += e.target.innerText;
+}
+
+document.querySelector('.result')
+    .addEventListener('click', function(){
+ display.value = eval(display.value);
+});
+
+document.querySelector('.clear-all')
+    .addEventListener('click', function(){
+    display.value = "";
+});
